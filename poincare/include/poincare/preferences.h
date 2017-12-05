d5c305bdc52e7efbdb34c107bc29bdc160f01e59
Lionel Debroux <lionel_debroux@yahoo.fr>
2017-12-05 21:39:29 +0100

    [poincare, escher, kandinsky, apps] Several easy, large optimizations on (mostly final, some singleton) classes with trivial constructors, getters and/or setters.
    Signed-off-by: Lionel Debroux <lionel_debroux@yahoo.fr>

--- poincare/include/poincare/preferences.h
+++ poincare/include/poincare/preferences.h
@@ -2,26 +2,51 @@
 #define POINCARE_PREFERENCES_H
 
 #include <poincare/expression.h>
+#include <poincare/print_float.h>
 
 namespace Poincare {
 
 class Preferences {
 public:
-  Preferences();
-  static Preferences * sharedPreferences();
-  Expression::AngleUnit angleUnit() const;
-  void setAngleUnit(Expression::AngleUnit angleUnit);
-  PrintFloat::Mode displayMode() const;
-  void setDisplayMode(PrintFloat::Mode mode);
-  Expression::ComplexFormat complexFormat() const;
-  void setComplexFormat(Expression::ComplexFormat complexFormat);
-  char numberOfSignificantDigits() const;
-  void setNumberOfSignificantDigits(char numberOfSignificantDigits);
+  static Preferences * sharedPreferences() {
+    return &s_preferences;
+  }
+  Expression::AngleUnit angleUnit() const {
+    return m_angleUnit;
+  }
+  void setAngleUnit(Expression::AngleUnit angleUnit) {
+    m_angleUnit = angleUnit;
+  }
+  PrintFloat::Mode displayMode() const {
+    return m_displayMode;
+  }
+  void setDisplayMode(PrintFloat::Mode mode) {
+    m_displayMode = mode;
+  }
+  Expression::ComplexFormat complexFormat() const {
+    return m_complexFormat;
+  }
+  void setComplexFormat(Expression::ComplexFormat complexFormat) {
+    m_complexFormat = complexFormat;
+  }
+  char numberOfSignificantDigits() const {
+    return m_numberOfSignificantDigits;
+  }
+  void setNumberOfSignificantDigits(char numberOfSignificantDigits) {
+    m_numberOfSignificantDigits = numberOfSignificantDigits;
+  }
 private:
+  Preferences() :
+    m_angleUnit(Expression::AngleUnit::Degree),
+    m_displayMode(PrintFloat::Mode::Decimal),
+    m_complexFormat(Expression::ComplexFormat::Cartesian),
+    m_numberOfSignificantDigits(PrintFloat::k_numberOfPrintedSignificantDigits) {
+  }
   Expression::AngleUnit m_angleUnit;
   PrintFloat::Mode m_displayMode;
   Expression::ComplexFormat m_complexFormat;
   char m_numberOfSignificantDigits;
+  static Preferences s_preferences;
 };
 
 }
